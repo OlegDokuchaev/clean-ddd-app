73b80d78a865472e122ac829ab07b7a38054319d
dokuchaev <dokuchaev@akc.ru>
2025-02-28 17:34:25 +0300

    feat(order service mocks): add create order saga publisher mock

---
+++ order/internal/mocks/order/saga/create_order/publisher_mock.go
@@ -0,0 +1,41 @@
+package create_order
+
+import (
+	"context"
+	"github.com/stretchr/testify/mock"
+	createOrderSaga "order/internal/application/order/saga/create_order"
+)
+
+type PublisherMock struct {
+	mock.Mock
+}
+
+func (p *PublisherMock) PublishReserveItemsCmd(ctx context.Context, cmd createOrderSaga.ReserveItemsCmd) error {
+	args := p.Called(ctx, cmd)
+	return args.Error(0)
+}
+
+func (p *PublisherMock) PublishReleaseItemsCmd(ctx context.Context, cmd createOrderSaga.ReleaseItemsCmd) error {
+	args := p.Called(ctx, cmd)
+	return args.Error(0)
+}
+
+func (p *PublisherMock) PublishCancelOutOfStockCmd(ctx context.Context, cmd createOrderSaga.CancelOutOfStockCmd) error {
+	args := p.Called(ctx, cmd)
+	return args.Error(0)
+}
+
+func (p *PublisherMock) PublishAssignCourierCmd(ctx context.Context, cmd createOrderSaga.AssignCourierCmd) error {
+	args := p.Called(ctx, cmd)
+	return args.Error(0)
+}
+
+func (p *PublisherMock) PublishBeginDeliveryCmd(ctx context.Context, cmd createOrderSaga.BeginDeliveryCmd) error {
+	args := p.Called(ctx, cmd)
+	return args.Error(0)
+}
+
+func (p *PublisherMock) PublishCancelCourierNotFoundCmd(ctx context.Context, cmd createOrderSaga.CancelCourierNotFoundCmd) error {
+	args := p.Called(ctx, cmd)
+	return args.Error(0)
+}
