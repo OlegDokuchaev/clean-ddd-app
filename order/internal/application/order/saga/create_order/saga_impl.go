b7258056d09b7203d5c982b13a4eff153bf252a9
dokuchaev <dokuchaev@akc.ru>
2025-02-28 16:15:48 +0300

    feat(order service application): add create order saga implementation

---
+++ order/internal/application/order/saga/create_order/saga_impl.go
@@ -0,0 +1,67 @@
+package create_order
+
+import (
+	"context"
+	"github.com/google/uuid"
+)
+
+type SagaImpl struct {
+	publisher Publisher
+}
+
+func New(publisher Publisher) Saga {
+	return &SagaImpl{
+		publisher: publisher,
+	}
+}
+
+func (s *SagaImpl) HandleItemsReserved(ctx context.Context, event ItemsReserved) error {
+	cmd := AssignCourierCmd{
+		OrderID: event.OrderID,
+		Cmd: Cmd{
+			ID: uuid.New(),
+		},
+	}
+	return s.publisher.PublishAssignCourierCmd(ctx, cmd)
+}
+
+func (s *SagaImpl) HandleItemsReservationFailed(ctx context.Context, event ItemsReservationFailed) error {
+	cmd := CancelOutOfStockCmd{
+		OrderID: event.OrderID,
+		Cmd: Cmd{
+			ID: uuid.New(),
+		},
+	}
+	return s.publisher.PublishCancelOutOfStockCmd(ctx, cmd)
+}
+
+func (s *SagaImpl) HandleCourierAssignmentFailed(ctx context.Context, event CourierAssignmentFailed) error {
+	cmd := ReleaseItemsCmd{
+		OrderID: event.OrderID,
+		Cmd: Cmd{
+			ID: uuid.New(),
+		},
+	}
+	return s.publisher.PublishReleaseItemsCmd(ctx, cmd)
+}
+
+func (s *SagaImpl) HandleItemsReleased(ctx context.Context, event ItemsReleased) error {
+	cmd := CancelCourierNotFoundCmd{
+		OrderID: event.OrderID,
+		Cmd: Cmd{
+			ID: uuid.New(),
+		},
+	}
+	return s.publisher.PublishCancelCourierNotFoundCmd(ctx, cmd)
+}
+
+func (s *SagaImpl) HandleCourierAssigned(ctx context.Context, event CourierAssigned) error {
+	cmd := BeginDeliveryCmd{
+		OrderID:   event.OrderID,
+		CourierID: event.CourierID,
+		Cmd: Cmd{
+			ID: uuid.New(),
+		},
+	}
+	return s.publisher.PublishBeginDeliveryCmd(ctx, cmd)
+}
