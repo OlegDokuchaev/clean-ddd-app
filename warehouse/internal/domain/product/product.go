542031139ed84528e1bcd253ffbdd911bcd555dc
dokuchaev <dokuchaev@akc.ru>
2025-03-02 16:45:26 +0300

    feat(warehouse service domain): add product entity

---
+++ warehouse/internal/domain/product/product.go
@@ -0,0 +1,14 @@
+package product
+
+import (
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+	"time"
+)
+
+type Product struct {
+	ID      uuid.UUID
+	Name    string
+	Price   decimal.Decimal
+	Created time.Time
+}
