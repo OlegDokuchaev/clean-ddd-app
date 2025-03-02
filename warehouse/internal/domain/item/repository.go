8712c93be0f9575d25ecb9ee284c0fe625f377fa
dokuchaev <dokuchaev@akc.ru>
2025-03-02 21:03:14 +0300

    feat(warehouse service domain): add item repository

---
+++ warehouse/internal/domain/item/repository.go
@@ -0,0 +1,13 @@
+package item
+
+import (
+	"context"
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	Create(ctx context.Context, item *Item) error
+	Update(ctx context.Context, item *Item) error
+	GetAllByIDs(ctx context.Context, itemIDs ...uuid.UUID) ([]*Item, error)
+	GetByID(ctx context.Context, itemID uuid.UUID) (*Item, error)
+}
