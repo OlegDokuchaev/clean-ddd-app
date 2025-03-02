6ade68597c8af574fb82cbfc57bfd76823fd503e
dokuchaev <dokuchaev@akc.ru>
2025-03-02 16:46:45 +0300

    feat(warehouse service domain): add product fabric

---
+++ warehouse/internal/domain/product/fabric.go
@@ -0,0 +1,30 @@
+package product
+
+import (
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+	"strings"
+	"time"
+	domain "warehouse/internal/domain/common"
+)
+
+func Create(name string, price decimal.Decimal) (*Product, []*domain.Event, error) {
+	if price.LessThanOrEqual(decimal.Zero) {
+		return nil, []*domain.Event{}, ErrInvalidProductPrice
+	}
+	if strings.TrimSpace(name) == "" {
+		return nil, []*domain.Event{}, ErrInvalidProductName
+	}
+
+	product := &Product{
+		ID:      uuid.New(),
+		Name:    name,
+		Price:   price,
+		Created: time.Now(),
+	}
+	event := newCreatedEvent(CreatedPayload{
+		ProductID: product.ID,
+	})
+
+	return product, []*domain.Event{&event}, nil
+}
