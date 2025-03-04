5b320a7ae4d562d79eb93dd20c9ac6680bee4f75
dokuchaev <dokuchaev@akc.ru>
2025-03-04 19:35:16 +0300

    feat(courier service domain): add courier errors

---
+++ courier/internal/domain/error.go
@@ -0,0 +1,9 @@
+package domain
+
+import "errors"
+
+var (
+	ErrInvalidCourierPassword = errors.New("invalid courier password")
+	ErrInvalidCourierPhone    = errors.New("invalid courier phone")
+	ErrInvalidCourierName     = errors.New("invalid courier name")
+)
