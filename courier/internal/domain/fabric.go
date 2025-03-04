c4e68bdf97325398dee3726c274c447da35191bb
dokuchaev <dokuchaev@akc.ru>
2025-03-04 19:35:34 +0300

    feat(courier service domain): add courier fabric

---
+++ courier/internal/domain/fabric.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+	"github.com/google/uuid"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var phoneRegex = regexp.MustCompile(`^\+?[0-9]{10,15}$`)
+
+func validatePhone(phone string) bool {
+	return phoneRegex.MatchString(phone)
+}
+
+func validateName(name string) bool {
+	return strings.TrimSpace(name) != ""
+}
+
+func Create(name, phone, password string) (*Courier, error) {
+	if !validateName(name) {
+		return nil, ErrInvalidCourierName
+	}
+	if !validatePhone(phone) {
+		return nil, ErrInvalidCourierPhone
+	}
+
+	c := &Courier{
+		ID:      uuid.New(),
+		Name:    name,
+		Phone:   phone,
+		Created: time.Now(),
+	}
+	if err := c.SetPassword(password); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
