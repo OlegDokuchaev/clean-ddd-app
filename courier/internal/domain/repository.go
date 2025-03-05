86c4aafca281cb2240a68e0ba3de42ec20b46e23
dokuchaev <dokuchaev@akc.ru>
2025-03-05 10:03:34 +0300

    feat(courier service domain): add courier repository

---
+++ courier/internal/domain/repository.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"context"
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	Create(ctx context.Context, courier *Courier) error
+	GetByID(ctx context.Context, orderID uuid.UUID) (*Courier, error)
+	GetAll(ctx context.Context) ([]*Courier, error)
+}
