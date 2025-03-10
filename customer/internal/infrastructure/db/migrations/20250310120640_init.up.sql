7f500fe8beb82a33aecbf366e6258d9bcb66ce5e
dokuchaev <dokuchaev@akc.ru>
2025-03-10 15:18:51 +0300

    feat(customer service infrastructure): update db migrations

--- customer/internal/infrastructure/db/migrations/20250310120640_init.up.sql
+++ customer/internal/infrastructure/db/migrations/20250310120640_init.up.sql
@@ -4,7 +4,6 @@ CREATE TABLE customers (
     id UUID PRIMARY KEY,
     name TEXT NOT NULL,
     phone TEXT UNIQUE NOT NULL,
-    email TEXT NOT NULL,
     password BYTEA NOT NULL,
     created TIMESTAMPTZ NOT NULL
 );
