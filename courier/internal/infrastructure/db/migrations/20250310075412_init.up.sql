dcaa5d920aa0b72ddf12117b68e3b77618ed7742
dokuchaev <dokuchaev@akc.ru>
2025-03-10 11:01:48 +0300

    feat(courier service infrastructure): add db migrations

---
+++ courier/internal/infrastructure/db/migrations/20250310075412_init.up.sql
@@ -0,0 +1,11 @@
+begin;
+
+CREATE TABLE couriers (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    password BYTEA NOT NULL,
+    created TIMESTAMPTZ NOT NULL
+);
+
+end;
