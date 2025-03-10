1178bfec5a26c34529835b6a627567b58fb3172f
dokuchaev <dokuchaev@akc.ru>
2025-03-10 15:40:56 +0300

    feat(warehouse service infrastructure): update item repository mappers

--- warehouse/internal/infrastructure/repository/item/mappers.go
+++ warehouse/internal/infrastructure/repository/item/mappers.go
@@ -6,29 +6,29 @@ import (
 	productRepository "warehouse/internal/infrastructure/repository/product"
 )
 
-func ToDomain(item *tables.Item) *itemDomain.Item {
+func ToDomain(model *tables.Item) *itemDomain.Item {
 	return &itemDomain.Item{
-		ID:      item.ID,
-		Count:   item.Count,
-		Version: item.Version,
-		Product: productRepository.ToDomain(item.Product),
+		ID:      model.ID,
+		Count:   model.Count,
+		Version: model.Version,
+		Product: productRepository.ToDomain(model.Product),
 	}
 }
 
-func ToDomains(items []*tables.Item) []*itemDomain.Item {
-	domains := make([]*itemDomain.Item, 0, len(items))
-	for _, item := range items {
+func ToDomains(model []*tables.Item) []*itemDomain.Item {
+	domains := make([]*itemDomain.Item, 0, len(model))
+	for _, item := range model {
 		domains = append(domains, ToDomain(item))
 	}
 	return domains
 }
 
-func ToModel(item *itemDomain.Item) *tables.Item {
+func ToModel(domain *itemDomain.Item) *tables.Item {
 	return &tables.Item{
-		ID:        item.ID,
-		ProductID: item.Product.ID,
-		Count:     item.Count,
-		Version:   item.Version,
-		Product:   productRepository.ToModel(item.Product),
+		ID:        domain.ID,
+		ProductID: domain.Product.ID,
+		Count:     domain.Count,
+		Version:   domain.Version,
+		Product:   productRepository.ToModel(domain.Product),
 	}
 }
