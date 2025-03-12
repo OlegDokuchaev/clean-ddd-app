cd2ddb57a6becd133acc494ef218e8599eff419b
dokuchaev <dokuchaev@akc.ru>
2025-03-12 09:22:13 +0300

    feat(order service tests): update db testutils

--- order/go.mod
+++ order/go.mod
@@ -55,6 +55,7 @@ require (
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/testcontainers/testcontainers-go v0.35.0 // indirect
+	github.com/testcontainers/testcontainers-go/modules/postgres v0.35.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
--- order/internal/tests/integration/repository/order_test.go
+++ order/internal/tests/integration/repository/order_test.go
@@ -5,7 +5,6 @@ package repository
 import (
 	"context"
 	orderDomain "order/internal/domain/order"
-	"order/internal/infrastructure/db"
 	"order/internal/infrastructure/db/migrations"
 	orderRepository "order/internal/infrastructure/repository/order"
 	"order/internal/tests/testutils"
@@ -24,19 +23,12 @@ type OrderRepositoryTestSuite struct {
 }
 
 func (s *OrderRepositoryTestSuite) SetupSuite() {
-	config, err := db.NewConfig()
-	require.NoError(s.T(), err)
-
 	mConfig, err := migrations.NewConfig()
 	require.NoError(s.T(), err)
 
 	s.ctx = context.Background()
 
-	testDB, err := testutils.NewTestDB(s.ctx, config, mConfig)
-	require.NoError(s.T(), err)
-	s.testDB = testDB
-
-	err = s.testDB.Migrations.Up()
+	s.testDB, err = testutils.NewTestDB(s.ctx, mConfig)
 	require.NoError(s.T(), err)
 }
 
