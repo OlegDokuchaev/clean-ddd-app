f40a506e8c9821f3b82c2bef09e7ea093ada5b5f
dokuchaev <dokuchaev@akc.ru>
2025-03-13 09:15:01 +0300

    feat(courier service tests): update testutils

--- courier/internal/tests/integration/repository/courier_test.go
+++ courier/internal/tests/integration/repository/courier_test.go
@@ -35,7 +35,7 @@ func (s *CourierRepositoryTestSuite) SetupSuite() {
 
 func (s *CourierRepositoryTestSuite) TearDownSuite() {
 	if s.testDB != nil {
-		err := s.testDB.Container.Terminate(s.ctx)
+		err := s.testDB.Close(s.ctx)
 		require.NoError(s.T(), err)
 	}
 }
