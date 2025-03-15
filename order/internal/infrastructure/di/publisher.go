56f87643ce1722b99d092d057cd1cdb8ac68ee57
dokuchaev <dokuchaev@akc.ru>
2025-03-15 15:43:25 +0300

    feat(order service infrastructure): add di publisher

---
+++ order/internal/infrastructure/di/publisher.go
@@ -0,0 +1,28 @@
+package di
+
+import (
+	"order/internal/infrastructure/messaging"
+	createOrderPublisher "order/internal/infrastructure/publisher/saga/create_order"
+
+	"go.uber.org/fx"
+)
+
+var PublisherModule = fx.Provide(
+	messaging.NewConfig,
+	fx.Annotate(
+		messaging.NewOrderCommandWriter,
+		fx.ParamTags(`name:"orderWriter"`),
+	),
+	fx.Annotate(
+		messaging.NewWarehouseCommandWriter,
+		fx.ParamTags(`name:"warehouseWriter"`),
+	),
+	fx.Annotate(
+		messaging.NewCourierCommandWriter,
+		fx.ParamTags(`name:"courierWriter"`),
+	),
+	fx.Annotate(
+		createOrderPublisher.NewPublisher,
+		fx.ParamTags(`name:"warehouseWriter"`, `name:"orderWriter"`, `name:"courierWriter"`),
+	),
+)
