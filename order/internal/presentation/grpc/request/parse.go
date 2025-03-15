7f6af79c6d14ac9df47a00a6c6e1bc71f821cda6
dokuchaev <dokuchaev@akc.ru>
2025-03-15 19:43:58 +0300

    feat(order service presentation): add grpc request parse

---
+++ order/internal/presentation/grpc/request/parse.go
@@ -0,0 +1,19 @@
+package request
+
+import (
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+	"order/internal/presentation/grpc/response"
+)
+
+func ParseUUID(key string) (uuid.UUID, error) {
+	if id, err := uuid.Parse(key); err != nil {
+		return uuid.Nil, response.ErrInvalidID
+	} else {
+		return id, nil
+	}
+}
+
+func ParseDecimal(key float64) decimal.Decimal {
+	return decimal.NewFromFloat(key)
+}
