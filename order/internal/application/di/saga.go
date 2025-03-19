9d00bd44a5da4ddec83e74323858db8b78feaeec
dokuchaev <dokuchaev@akc.ru>
2025-03-19 11:52:59 +0300

    feat(order service applications): update saga di

--- order/internal/application/di/saga.go
+++ order/internal/application/di/saga.go
@@ -1,13 +1,18 @@
 package di
 
 import (
-	"go.uber.org/fx"
 	createOrder "order/internal/application/order/saga/create_order"
+
+	"go.uber.org/fx"
 )
 
 var SagaModule = fx.Provide(
 	fx.Annotate(
 		createOrder.New,
 		fx.As(new(createOrder.Saga)),
+	),
+	fx.Annotate(
+		createOrder.NewManager,
+		fx.As(new(createOrder.Manager)),
 	),
 )
