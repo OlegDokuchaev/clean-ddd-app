5a29949ec4d0c60a70b6486bd5fcce9be459f0d8
dokuchaev <dokuchaev@akc.ru>
2025-03-21 10:42:55 +0300

    fix(warehouse service infrastructure): update messaging writers

--- warehouse/internal/infrastructure/messaging/writer.go
+++ warehouse/internal/infrastructure/messaging/writer.go
@@ -8,3 +8,10 @@ func NewWarehouseCmdResWriter(config *Config) *kafka.Writer {
 		Topic: config.WarehouseCmdResTopic,
 	}
 }
+
+func NewProductEventWriter(config *Config) *kafka.Writer {
+	return &kafka.Writer{
+		Addr:  kafka.TCP(config.Address),
+		Topic: config.ProductEventTopic,
+	}
+}
