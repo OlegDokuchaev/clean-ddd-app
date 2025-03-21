bd48d4e7cbf53aea431ffd2d8f73af6d008413ef
dokuchaev <dokuchaev@akc.ru>
2025-03-21 11:22:36 +0300

    fix(warehouse service presentation): add events reader

---
+++ warehouse/internal/presentation/events/reader.go
@@ -0,0 +1,160 @@
+package events
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Reader interface {
+	Start(ctx context.Context) error
+	Read(ctx context.Context) (*Event, error)
+	Stop() error
+}
+
+type ReaderImpl struct {
+	reader    *kafka.Reader
+	eventChan chan *Event
+	errorChan chan error
+
+	cancelCtx  context.Context
+	cancelFunc context.CancelFunc
+
+	wg      sync.WaitGroup
+	mu      sync.Mutex
+	started bool
+}
+
+func NewReader(reader *kafka.Reader) *ReaderImpl {
+	return &ReaderImpl{
+		reader: reader,
+	}
+}
+
+func (r *ReaderImpl) Start(ctx context.Context) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if r.started {
+		return errors.New("event reader is already started")
+	}
+
+	r.eventChan = make(chan *Event, 1)
+	r.errorChan = make(chan error, 1)
+
+	r.cancelCtx, r.cancelFunc = context.WithCancel(ctx)
+
+	r.started = true
+	log.Println("Starting event reader...")
+
+	r.wg.Add(1)
+
+	go func() {
+		defer r.wg.Done()
+		r.readEvents(r.cancelCtx)
+	}()
+
+	return nil
+}
+
+func (r *ReaderImpl) Stop() error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if !r.started {
+		return errors.New("event reader is already stopped or was not started")
+	}
+
+	log.Printf("Stopping event reader...")
+
+	if r.cancelFunc != nil {
+		r.cancelFunc()
+	}
+
+	r.wg.Wait()
+
+	close(r.eventChan)
+	close(r.errorChan)
+
+	r.started = false
+	log.Printf("Event reader has been stopped.")
+
+	return nil
+}
+
+func (r *ReaderImpl) readEvents(ctx context.Context) {
+	defer func() {
+		log.Printf("Event reader goroutine completed")
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("Context canceled, stopping event reader")
+			return
+		default:
+			msg, err := r.reader.ReadMessage(ctx)
+
+			if ctx.Err() != nil {
+				continue
+			}
+
+			if err != nil {
+				select {
+				case r.errorChan <- fmt.Errorf("error reading message: %w", err):
+				case <-ctx.Done():
+				}
+				continue
+			}
+
+			event, err := parseEvent(msg.Value)
+			if err != nil {
+				select {
+				case r.errorChan <- fmt.Errorf("error parsing message: %w", err):
+				case <-ctx.Done():
+				}
+				continue
+			}
+
+			select {
+			case r.eventChan <- event:
+				log.Printf("Event received: %s, type: %s", event.ID, event.Name)
+			case <-ctx.Done():
+			}
+		}
+	}
+}
+
+func (r *ReaderImpl) Read(ctx context.Context) (*Event, error) {
+	select {
+	case cmd, ok := <-r.eventChan:
+		if !ok {
+			return nil, fmt.Errorf("event channel closed")
+		}
+		return cmd, nil
+
+	case err, ok := <-r.errorChan:
+		if !ok {
+			return nil, fmt.Errorf("error channel closed")
+		}
+		return nil, err
+
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+var _ Reader = (*ReaderImpl)(nil)
+
+func parseEvent(data []byte) (*Event, error) {
+	var msg Event
+	if err := json.Unmarshal(data, &msg); err != nil {
+		return nil, fmt.Errorf("error deserializing message: %w", err)
+	}
+	return &msg, nil
+}
