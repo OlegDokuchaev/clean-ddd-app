3ff950848eb8c03a52a7b02350960513ee65ea0c
dokuchaev <dokuchaev@akc.ru>
2025-03-21 10:12:35 +0300

    fix(warehouse service presentation): add command handler

---
+++ warehouse/internal/presentation/commands/handler.go
@@ -0,0 +1,69 @@
+package commands
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	itemApplication "warehouse/internal/application/item"
+)
+
+type Handler interface {
+	Handle(ctx context.Context, cmdMsg *CmdMessage) (*ResMessage, error)
+}
+
+type HandlerImpl struct {
+	usecase itemApplication.UseCase
+}
+
+func NewHandler(usecase itemApplication.UseCase) *HandlerImpl {
+	return &HandlerImpl{usecase: usecase}
+}
+
+func (h *HandlerImpl) Handle(ctx context.Context, cmdMsg *CmdMessage) (*ResMessage, error) {
+	payloadBytes, err := json.Marshal(cmdMsg.Payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal payload: %w", err)
+	}
+
+	switch cmdMsg.Name {
+	case ReserveItemsCmdName:
+		var cmd ReserveItemsCmd
+		if err = json.Unmarshal(payloadBytes, &cmd); err != nil {
+			return nil, fmt.Errorf("failed to parse ReserveItemsCmd: %w", err)
+		}
+		return h.onReserveItems(ctx, cmd), nil
+
+	case ReleaseItemsCmdName:
+		var cmd ReleaseItemsCmd
+		if err = json.Unmarshal(payloadBytes, &cmd); err != nil {
+			return nil, fmt.Errorf("failed to parse ReleaseItemsCmd: %w", err)
+		}
+		return h.onReleaseItems(ctx, cmd), nil
+	}
+
+	return nil, fmt.Errorf("unknown command: %s", cmdMsg.Name)
+}
+
+func (h *HandlerImpl) onReserveItems(ctx context.Context, cmd ReserveItemsCmd) *ResMessage {
+	data := toReserveItemsDto(cmd)
+
+	err := h.usecase.Reserve(ctx, data)
+
+	if err != nil {
+		return toItemsReservationFailed(cmd.OrderID)
+	}
+	return toItemsReserved(cmd.OrderID)
+}
+
+func (h *HandlerImpl) onReleaseItems(ctx context.Context, cmd ReleaseItemsCmd) *ResMessage {
+	data := toReleaseItemsDto(cmd)
+
+	err := h.usecase.Release(ctx, data)
+
+	if err != nil {
+		return nil
+	}
+	return toItemsReleased(cmd.OrderID)
+}
+
+var _ Handler = (*HandlerImpl)(nil)
