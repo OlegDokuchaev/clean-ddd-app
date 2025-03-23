c5b5dbbddd2d05fb7454189efd661f6150853b49
dokuchaev <dokuchaev@akc.ru>
2025-03-23 23:23:45 +0300

    feat(customer service presentation): add grpc config

---
+++ customer/internal/presentation/grpc/config.go
@@ -0,0 +1,19 @@
+package customerv1
+
+import (
+	"fmt"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Port string `envconfig:"GRPC_PORT" required:"true"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, fmt.Errorf("failed to load grpc config: %w", err)
+	}
+	return &cfg, nil
+}
