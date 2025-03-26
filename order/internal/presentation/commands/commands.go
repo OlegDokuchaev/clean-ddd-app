efd2038405ec046100fb317750befd229ed6c108
dokuchaev <dokuchaev@akc.ru>
2025-03-26 14:08:01 +0300

    feat(order service presentation): add commands

---
+++ order/internal/presentation/commands/commands.go
@@ -0,0 +1,35 @@
+package commands
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+)
+
+const (
+	CancelOutOfStockCmdName      CmdMessageName = "create_order.cancel_out_of_stock"
+	BeginDeliveryCmdName         CmdMessageName = "create_order.begin_delivery"
+	CancelCourierNotFoundCmdName CmdMessageName = "create_order.cancel_courier_not_found"
+)
+
+type (
+	CmdMessageName string
+
+	CmdMessage struct {
+		ID      uuid.UUID
+		Name    CmdMessageName
+		Payload json.RawMessage
+	}
+)
+
+type CancelOutOfStockCmd struct {
+	OrderID uuid.UUID
+}
+
+type CancelCourierNotFoundCmd struct {
+	OrderID uuid.UUID
+}
+
+type BeginDeliveryCmd struct {
+	OrderID   uuid.UUID
+	CourierID uuid.UUID
+}
