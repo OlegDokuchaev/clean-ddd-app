37766f04323f0b4c1bd49b6702f178444b1ce370
dokuchaev <dokuchaev@akc.ru>
2025-04-02 08:35:30 +0300

    feat(order service infrastructure): update logger

--- order/internal/infrastructure/logger/logger.go
+++ order/internal/infrastructure/logger/logger.go
@@ -4,6 +4,16 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type Level int
+
+const (
+	Debug Level = iota
+	Info
+	Warn
+	Error
+	Fatal
+)
+
 type Logger interface {
 	Info(msg string, fields map[string]any)
 	Warn(msg string, fields map[string]any)
@@ -12,6 +22,7 @@ type Logger interface {
 	Printf(format string, v ...any)
 	Println(v ...any)
 	Fatalf(format string, v ...any)
+	Log(level Level, msg string, fields map[string]any)
 }
 
 type LoggerImpl struct {
@@ -22,36 +33,27 @@ func NewLogger(log *logrus.Logger) Logger {
 	return &LoggerImpl{log: log}
 }
 
-func (l *LoggerImpl) Info(msg string, fields map[string]any) {
-	entry := l.log.WithFields(logrus.Fields{})
+func (l *LoggerImpl) logWithFields(fields map[string]any) *logrus.Entry {
 	if fields != nil {
-		entry = entry.WithFields(fields)
+		return l.log.WithFields(fields)
 	}
-	entry.Info(msg)
+	return logrus.NewEntry(l.log)
+}
+
+func (l *LoggerImpl) Info(msg string, fields map[string]any) {
+	l.logWithFields(fields).Info(msg)
 }
 
 func (l *LoggerImpl) Warn(msg string, fields map[string]any) {
-	entry := l.log.WithFields(logrus.Fields{})
-	if fields != nil {
-		entry = entry.WithFields(fields)
-	}
-	entry.Warn(msg)
+	l.logWithFields(fields).Warn(msg)
 }
 
 func (l *LoggerImpl) Error(msg string, fields map[string]any) {
-	entry := l.log.WithFields(logrus.Fields{})
-	if fields != nil {
-		entry = entry.WithFields(fields)
-	}
-	entry.Error(msg)
+	l.logWithFields(fields).Error(msg)
 }
 
 func (l *LoggerImpl) Debug(msg string, fields map[string]any) {
-	entry := l.log.WithFields(logrus.Fields{})
-	if fields != nil {
-		entry = entry.WithFields(fields)
-	}
-	entry.Debug(msg)
+	l.logWithFields(fields).Debug(msg)
 }
 
 func (l *LoggerImpl) Printf(format string, v ...any) {
@@ -64,6 +66,23 @@ func (l *LoggerImpl) Println(v ...any) {
 
 func (l *LoggerImpl) Fatalf(format string, v ...any) {
 	l.log.Fatalf(format, v...)
+}
+
+func (l *LoggerImpl) Log(level Level, msg string, fields map[string]any) {
+	switch level {
+	case Debug:
+		l.Debug(msg, fields)
+	case Info:
+		l.Info(msg, fields)
+	case Warn:
+		l.Warn(msg, fields)
+	case Error:
+		l.Error(msg, fields)
+	case Fatal:
+		l.Fatalf("%s", msg)
+	default:
+		l.Info(msg, fields)
+	}
 }
 
 var _ Logger = (*LoggerImpl)(nil)
