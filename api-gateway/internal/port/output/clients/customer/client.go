90c00e150c39b909c34094877e6598f47d273f2d
dokuchaev <dokuchaev@akc.ru>
2025-04-04 11:25:58 +0300

    feat(api-gateway service domain): add customer dtos

--- api-gateway/internal/port/output/clients/customer/client.go
+++ api-gateway/internal/port/output/clients/customer/client.go
@@ -1,14 +1,14 @@
 package customer
 
 import (
-	customerUseCase "api-gateway/internal/domain/usecases/customer"
+	customerDto "api-gateway/internal/domain/dtos/customer"
 	"context"
 
 	"github.com/google/uuid"
 )
 
 type Client interface {
-	Register(ctx context.Context, data customerUseCase.RegisterDto) (uuid.UUID, error)
-	Login(ctx context.Context, data customerUseCase.LoginDto) (string, error)
+	Register(ctx context.Context, data customerDto.RegisterDto) (uuid.UUID, error)
+	Login(ctx context.Context, data customerDto.LoginDto) (string, error)
 	Authenticate(ctx context.Context, token string) (uuid.UUID, error)
 }
