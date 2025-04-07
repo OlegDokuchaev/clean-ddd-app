31e37be61ce63ea9b426ba0e6035ed82c8c174d1
dokuchaev <dokuchaev@akc.ru>
2025-04-07 16:05:10 +0300

    feat(api-gateway service adapter): add customer client config

---
+++ api-gateway/internal/adapter/output/clients/customer/config.go
@@ -0,0 +1,19 @@
+package customer
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Address string `envconfig:"CUSTOMER_ADDRESS" required:"true"`
+	Timeout int    `envconfig:"CUSTOMER_TIMEOUT" required:"true"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, fmt.Errorf("failed to load client config: %w", err)
+	}
+	return &cfg, nil
+}
