11045048a42e738e9054bcbe71cb89b84c9ac2fa
dokuchaev <dokuchaev@akc.ru>
2025-04-07 16:06:06 +0300

    feat(api-gateway service adapter): add customer client connection

---
+++ api-gateway/internal/adapter/output/clients/customer/connection.go
@@ -0,0 +1,47 @@
+package customer
+
+import (
+	customerGRPC "api-gateway/gen/customer/v1"
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/connectivity"
+	"google.golang.org/grpc/credentials/insecure"
+	"time"
+)
+
+func newConnection(config *Config) (*grpc.ClientConn, error) {
+	timeout := time.Duration(config.Timeout) * time.Second
+	target := "passthrough:///" + config.Address
+
+	conn, err := grpc.NewClient(
+		target,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithConnectParams(grpc.ConnectParams{MinConnectTimeout: timeout}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.Connect()
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	state := conn.GetState()
+	for state != connectivity.Ready {
+		if !conn.WaitForStateChange(ctx, state) {
+			return nil, ctx.Err()
+		}
+		state = conn.GetState()
+	}
+
+	return conn, nil
+}
+
+func NewGRPCClient(config *Config) (customerGRPC.CustomerAuthServiceClient, error) {
+	conn, err := newConnection(config)
+	if err != nil {
+		return nil, err
+	}
+	return customerGRPC.NewCustomerAuthServiceClient(conn), nil
+}
