14c5d40656e277ec0e2353375b0a7956255141cd
dokuchaev <dokuchaev@akc.ru>
2025-04-07 16:07:50 +0300

    feat(api-gateway service adapter): add warehouse client config

---
+++ api-gateway/internal/adapter/output/clients/warehouse/config.go
@@ -0,0 +1,19 @@
+package warehouse
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Address string `envconfig:"WAREHOUSE_ADDRESS" required:"true"`
+	Timeout int    `envconfig:"WAREHOUSE_TIMEOUT" required:"true"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, fmt.Errorf("failed to load client config: %w", err)
+	}
+	return &cfg, nil
+}
