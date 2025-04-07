4a68241744f59af86effb750a13db456a92cbb2b
dokuchaev <dokuchaev@akc.ru>
2025-04-07 16:23:17 +0300

    feat(api-gateway service adapter): add warehouse client connection

--- api-gateway/internal/adapter/output/clients/warehouse/client.go
+++ api-gateway/internal/adapter/output/clients/warehouse/client.go
@@ -1,7 +1,6 @@
 package warehouse
 
 import (
-	warehouseGRPC "api-gateway/gen/warehouse/v1"
 	"api-gateway/internal/adapter/output/clients/response"
 	warehouseDto "api-gateway/internal/domain/dtos/warehouse"
 	warehouseClient "api-gateway/internal/port/output/clients/warehouse"
@@ -12,24 +11,17 @@ import (
 )
 
 type ClientImpl struct {
-	itemClient    warehouseGRPC.ItemServiceClient
-	productClient warehouseGRPC.ProductServiceClient
+	clients *GRPCClients
 }
 
-func NewClient(
-	itemClient warehouseGRPC.ItemServiceClient,
-	productClient warehouseGRPC.ProductServiceClient,
-) warehouseClient.Client {
-	return &ClientImpl{
-		itemClient:    itemClient,
-		productClient: productClient,
-	}
+func NewClient(clients *GRPCClients) warehouseClient.Client {
+	return &ClientImpl{clients: clients}
 }
 
 func (c *ClientImpl) ReserveItems(ctx context.Context, items []warehouseDto.ItemInfoDto) error {
 	request := toReserveItemRequest(items)
 
-	_, err := c.itemClient.ReserveItem(ctx, request)
+	_, err := c.clients.Item.ReserveItem(ctx, request)
 	if err != nil {
 		return response.ParseGRPCError(err)
 	}
@@ -40,7 +32,7 @@ func (c *ClientImpl) ReserveItems(ctx context.Context, items []warehouseDto.Item
 func (c *ClientImpl) ReleaseItems(ctx context.Context, items []warehouseDto.ItemInfoDto) error {
 	request := toReleaseItemRequest(items)
 
-	_, err := c.itemClient.ReleaseItem(ctx, request)
+	_, err := c.clients.Item.ReleaseItem(ctx, request)
 	if err != nil {
 		return response.ParseGRPCError(err)
 	}
@@ -51,7 +43,7 @@ func (c *ClientImpl) ReleaseItems(ctx context.Context, items []warehouseDto.Item
 func (c *ClientImpl) CreateProduct(ctx context.Context, data warehouseDto.CreateProductDto) (uuid.UUID, error) {
 	request := toCreateProductRequest(data)
 
-	resp, err := c.productClient.CreateProduct(ctx, request)
+	resp, err := c.clients.Product.CreateProduct(ctx, request)
 	if err != nil {
 		return uuid.Nil, response.ParseGRPCError(err)
 	}
@@ -65,7 +57,7 @@ func (c *ClientImpl) CreateProduct(ctx context.Context, data warehouseDto.Create
 }
 
 func (c *ClientImpl) GetAllItems(ctx context.Context) ([]*warehouseDto.ItemDto, error) {
-	resp, err := c.itemClient.GetAllItems(ctx, &emptypb.Empty{})
+	resp, err := c.clients.Item.GetAllItems(ctx, &emptypb.Empty{})
 	if err != nil {
 		return nil, response.ParseGRPCError(err)
 	}
