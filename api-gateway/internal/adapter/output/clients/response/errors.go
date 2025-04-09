a9c0a18651ece52b64716c1bd16ab604bde31678
dokuchaev <dokuchaev@akc.ru>
2025-04-09 20:36:14 +0300

    feat(api-gateway service adapter): update clients error response

--- api-gateway/internal/adapter/output/clients/response/errors.go
+++ api-gateway/internal/adapter/output/clients/response/errors.go
@@ -2,23 +2,30 @@ package response
 
 import (
 	domainErrors "api-gateway/internal/domain/errors"
+	"net/http"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"net/http"
 )
 
 var grpcToHTTP = map[codes.Code]int{
+	codes.OK:                 http.StatusOK,
+	codes.Canceled:           http.StatusRequestTimeout,
+	codes.Unknown:            http.StatusInternalServerError,
 	codes.InvalidArgument:    http.StatusBadRequest,
+	codes.DeadlineExceeded:   http.StatusGatewayTimeout,
 	codes.NotFound:           http.StatusNotFound,
-	codes.Unauthenticated:    http.StatusUnauthorized,
+	codes.AlreadyExists:      http.StatusConflict,
 	codes.PermissionDenied:   http.StatusForbidden,
 	codes.ResourceExhausted:  http.StatusTooManyRequests,
-	codes.FailedPrecondition: http.StatusPreconditionFailed,
+	codes.FailedPrecondition: http.StatusBadRequest,
 	codes.Aborted:            http.StatusConflict,
+	codes.OutOfRange:         http.StatusBadRequest,
 	codes.Unimplemented:      http.StatusNotImplemented,
 	codes.Internal:           http.StatusInternalServerError,
-	codes.DataLoss:           http.StatusInternalServerError,
 	codes.Unavailable:        http.StatusServiceUnavailable,
+	codes.DataLoss:           http.StatusInternalServerError,
+	codes.Unauthenticated:    http.StatusUnauthorized,
 }
 
 var (
