289c61710bb382c5b931dfc7d933b78fb1907297
dokuchaev <dokuchaev@akc.ru>
2025-04-09 20:25:59 +0300

    feat(api-gateway service): add main api information

--- api-gateway/cmd/api-gateway/main.go
+++ api-gateway/cmd/api-gateway/main.go
@@ -7,6 +7,26 @@ import (
 	"go.uber.org/fx"
 )
 
+//	@title			Clean DDD App API Gateway
+//	@version		1.0
+//	@description	This is the API Gateway for the Clean DDD application.
+//	@BasePath		/
+
+//	@securityDefinitions.apikey	CourierBearerAuth
+//	@in							header
+//	@name						Authorization
+//	@description			    Courier's JWT token.
+
+//	@securityDefinitions.apikey	CustomerBearerAuth
+//	@in							header
+//	@name						Authorization
+//	@description				Customer's JWT token.
+
+//	@securityDefinitions.apikey	AdminAccessToken
+//	@in							header
+//	@name						X-Access-Token
+//	@description				Admin's access token.
+
 func main() {
 	fx.New(
 		di.ClientsModule,
