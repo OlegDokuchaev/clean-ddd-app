5d62dc25a0d09a75982b8e00ad3bed505381909c
dokuchaev <dokuchaev@akc.ru>
2025-04-14 10:44:07 +0300

    feat(warehouse service infrastructure): update product repository mappers

--- warehouse/internal/infrastructure/repository/product/mappers.go
+++ warehouse/internal/infrastructure/repository/product/mappers.go
@@ -3,6 +3,8 @@ package product
 import (
 	productDomain "warehouse/internal/domain/product"
 	"warehouse/internal/infrastructure/db/tables"
+
+	"github.com/google/uuid"
 )
 
 func ToDomain(model *tables.Product) *productDomain.Product {
@@ -11,6 +13,13 @@ func ToDomain(model *tables.Product) *productDomain.Product {
 		Name:    model.Name,
 		Price:   model.Price,
 		Created: model.Created,
+		Image:   toProductImageDomain(model.Image),
+	}
+}
+
+func toProductImageDomain(model tables.ProductImage) productDomain.Image {
+	return productDomain.Image{
+		Path: model.Path,
 	}
 }
 
@@ -20,5 +29,14 @@ func ToModel(domain *productDomain.Product) tables.Product {
 		Name:    domain.Name,
 		Price:   domain.Price,
 		Created: domain.Created,
+		Image:   toProductImageModel(domain.ID, domain.Image),
+	}
+}
+
+func toProductImageModel(productID uuid.UUID, domain productDomain.Image) tables.ProductImage {
+	return tables.ProductImage{
+		ID:        uuid.New(),
+		ProductID: productID,
+		Path:      domain.Path,
 	}
 }
