20a3a90f919afeee9f61d3b30d2dea45522f456e
dokuchaev <dokuchaev@akc.ru>
2025-04-16 18:55:18 +0300

    feat(warehouse service presentation): update grpc product image handler

--- warehouse/internal/presentation/grpc/handlers/product_image.go
+++ warehouse/internal/presentation/grpc/handlers/product_image.go
@@ -2,13 +2,14 @@ package handlers
 
 import (
 	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/emptypb"
 	"io"
 	productApplication "warehouse/internal/application/product"
 	warehousev1 "warehouse/internal/presentation/grpc"
 	"warehouse/internal/presentation/grpc/request"
 	"warehouse/internal/presentation/grpc/response"
+
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type ProductImageServiceHandler struct {
@@ -75,6 +76,57 @@ func (h *ProductImageServiceHandler) UploadImage(
 	}
 
 	return stream.SendAndClose(&emptypb.Empty{})
+}
+
+func (h *ProductImageServiceHandler) GetImage(
+	req *warehousev1.GetImageRequest,
+	stream grpc.ServerStreamingServer[warehousev1.GetImageResponse],
+) error {
+	productID, err := request.ParseUUID(req.GetProductId())
+	if err != nil {
+		return response.ParseError(err)
+	}
+
+	fileReader, contentType, err := h.usecase.GetByID(stream.Context(), productID)
+	if err != nil {
+		return response.ParseError(err)
+	}
+
+	// Send file info
+	headerMsg := &warehousev1.GetImageResponse{
+		Data: &warehousev1.GetImageResponse_Info{
+			Info: &warehousev1.ImageInfo{
+				ContentType: contentType,
+			},
+		},
+	}
+	if err = stream.Send(headerMsg); err != nil {
+		return response.ParseError(err)
+	}
+
+	// Send file data
+	buf := make([]byte, 32*1024)
+	for {
+		n, readErr := fileReader.Read(buf)
+		if n > 0 {
+			chunkMsg := &warehousev1.GetImageResponse{
+				Data: &warehousev1.GetImageResponse_ChunkData{
+					ChunkData: buf[:n],
+				},
+			}
+			if err = stream.Send(chunkMsg); err != nil {
+				return response.ParseError(err)
+			}
+		}
+		if readErr == io.EOF {
+			break
+		}
+		if readErr != nil {
+			return response.ParseError(err)
+		}
+	}
+
+	return nil
 }
 
 var _ warehousev1.ProductImageServiceServer = (*ProductImageServiceHandler)(nil)
