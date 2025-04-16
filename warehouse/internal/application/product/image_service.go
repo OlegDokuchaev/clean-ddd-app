eae64f71e218c1df16bb2e4ac8a23a229aaf483a
dokuchaev <dokuchaev@akc.ru>
2025-04-16 18:50:57 +0300

    fix(warehouse service application): add Get method in image service

--- warehouse/internal/application/product/image_service.go
+++ warehouse/internal/application/product/image_service.go
@@ -1,11 +1,13 @@
 package product
 
 import (
-	"golang.org/x/net/context"
 	"io"
+
+	"golang.org/x/net/context"
 )
 
 type ImageService interface {
 	Create(ctx context.Context) (string, error)
+	Get(ctx context.Context, path string) (fileReader io.ReadCloser, contentType string, err error)
 	Update(ctx context.Context, path string, fileReader io.Reader, contentType string) error
 }
