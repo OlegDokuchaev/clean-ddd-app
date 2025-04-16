29f26e39bdedeafcf12854d510610b4e03281135
dokuchaev <dokuchaev@akc.ru>
2025-04-16 09:41:16 +0300

    feat(warehouse service presentation): add product image handler

---
+++ warehouse/internal/presentation/grpc/handlers/product_image.go
@@ -0,0 +1,82 @@
+package handlers
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"io"
+	productApplication "warehouse/internal/application/product"
+	warehousev1 "warehouse/internal/presentation/grpc"
+	"warehouse/internal/presentation/grpc/request"
+	"warehouse/internal/presentation/grpc/response"
+)
+
+type ProductImageServiceHandler struct {
+	warehousev1.UnimplementedProductImageServiceServer
+
+	usecase productApplication.ImageUseCase
+}
+
+func NewProductImageServiceHandler(usecase productApplication.ImageUseCase) *ProductImageServiceHandler {
+	return &ProductImageServiceHandler{
+		usecase: usecase,
+	}
+}
+
+func (h *ProductImageServiceHandler) UploadImage(
+	stream grpc.ClientStreamingServer[warehousev1.UploadImageRequest, emptypb.Empty],
+) error {
+	var productID uuid.UUID
+	var contentType string
+
+	pr, pw := io.Pipe()
+	defer pr.Close()
+
+	errChan := make(chan error, 1)
+
+	go func() {
+		defer pw.Close()
+
+		for {
+			req, err := stream.Recv()
+			if err == io.EOF {
+				errChan <- nil
+				return
+			}
+			if err != nil {
+				errChan <- fmt.Errorf("error receiving stream chunk: %w", err)
+				return
+			}
+
+			if productID == uuid.Nil {
+				productID, err = request.ParseUUID(req.GetProductId())
+				if err != nil {
+					errChan <- err
+					return
+				}
+				contentType = req.GetContentType()
+			}
+
+			if data := req.GetChunkData(); len(data) > 0 {
+				if _, wErr := pw.Write(data); wErr != nil {
+					errChan <- fmt.Errorf("error writing chunk to pipe: %w", wErr)
+					return
+				}
+			}
+		}
+	}()
+
+	err := h.usecase.UpdateByID(stream.Context(), productID, pr, contentType)
+	if err != nil {
+		return response.ParseError(err)
+	}
+
+	if goroutineErr := <-errChan; goroutineErr != nil {
+		return response.ParseError(err)
+	}
+
+	return stream.SendAndClose(&emptypb.Empty{})
+}
+
+var _ warehousev1.ProductImageServiceServer = (*ProductImageServiceHandler)(nil)
