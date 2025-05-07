01478b808c5314ff2a4f46ff330032fb6336a2e8
dokuchaev <dokuchaev@akc.ru>
2025-05-07 15:15:08 +0300

    build(order service): update entrypoint.sh

--- order/entrypoint.sh
+++ order/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 echo "Running migrations..."
-DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable"
-migrate -path ${DB_MIGRATIONS_PATH} -database "$DATABASE_URL" up
+migrate -source "${DB_MIGRATIONS_PATH}" -database "${DB_URI}/${DB_NAME}" up
 
 echo "Starting the app..."
 exec ./main
