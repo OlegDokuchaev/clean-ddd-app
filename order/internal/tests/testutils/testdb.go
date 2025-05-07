8afe8449fbbc53bf4e699c1c6cdd3e73a9c8c060
dokuchaev <dokuchaev@akc.ru>
2025-05-07 15:09:10 +0300

    feat(order service tests): update testdb utils

--- order/internal/tests/testutils/testdb.go
+++ order/internal/tests/testutils/testdb.go
@@ -4,28 +4,25 @@ import (
 	"context"
 	"fmt"
 	"order/internal/infrastructure/db/migrations"
-	"time"
 
 	"github.com/golang-migrate/migrate/v4"
-	migratePostgres "github.com/golang-migrate/migrate/v4/database/postgres"
+	migrateMongo "github.com/golang-migrate/migrate/v4/database/mongodb"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/testcontainers/testcontainers-go"
-	postgresContainer "github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	mongoContainer "github.com/testcontainers/testcontainers-go/modules/mongodb"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 const (
-	TestDbName = "name"
-	TestDbUser = "user"
-	TestDbPass = "password"
+	TestDbName         = "name"
+	TestCollectionName = "orders"
 )
 
 type TestDB struct {
-	DB        *gorm.DB
-	container testcontainers.Container
+	Collection *mongo.Collection
+	container  testcontainers.Container
 }
 
 func (d *TestDB) Close(ctx context.Context) error {
@@ -33,16 +30,7 @@ func (d *TestDB) Close(ctx context.Context) error {
 }
 
 func setupDBContainer(ctx context.Context) (testcontainers.Container, error) {
-	return postgresContainer.Run(ctx,
-		"postgres:16-alpine",
-		postgresContainer.WithDatabase(TestDbName),
-		postgresContainer.WithUsername(TestDbUser),
-		postgresContainer.WithPassword(TestDbPass),
-		testcontainers.WithWaitStrategy(
-			wait.ForLog("database system is ready to accept connections").
-				WithOccurrence(2).
-				WithStartupTimeout(5*time.Second)),
-	)
+	return mongoContainer.Run(ctx, "mongo:6")
 }
 
 func createDSN(ctx context.Context, container testcontainers.Container) (string, error) {
@@ -51,59 +39,49 @@ func createDSN(ctx context.Context, container testcontainers.Container) (string,
 		return "", err
 	}
 
-	port, err := container.MappedPort(ctx, "5432/tcp")
+	port, err := container.MappedPort(ctx, "27017/tcp")
 	if err != nil {
 		return "", err
 	}
 
 	return fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port.Int(), TestDbUser, TestDbPass, TestDbName,
+		"mongodb://%s:%d",
+		host, port.Int(),
 	), nil
 }
 
-func initGORM(dsn string) (*gorm.DB, error) {
-	return gorm.Open(
-		postgres.Open(dsn),
-		&gorm.Config{
-			Logger: logger.Default.LogMode(logger.Silent),
-		},
-	)
-}
-
-func createGORM(ctx context.Context, container testcontainers.Container) (*gorm.DB, error) {
+func createMongo(ctx context.Context, container testcontainers.Container) (*mongo.Client, error) {
 	dsn, err := createDSN(ctx, container)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create DSN: %w", err)
+		return nil, err
 	}
 
-	db, err := initGORM(dsn)
+	clientOptions := options.Client().ApplyURI(dsn)
+	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		return nil, fmt.Errorf("failed to init GORM: %w", err)
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
 	}
 
-	return db, nil
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
+	return client, nil
 }
 
-func setupMigrations(db *gorm.DB, config *migrations.Config) (*migrate.Migrate, error) {
-	sqlDB, err := db.DB()
+func setupMigrations(db *mongo.Client, config *migrations.Config) (*migrate.Migrate, error) {
+	driver, err := migrateMongo.WithInstance(db, &migrateMongo.Config{DatabaseName: TestDbName})
 	if err != nil {
 		return nil, err
 	}
-
-	driver, err := migratePostgres.WithInstance(sqlDB, &migratePostgres.Config{})
-	if err != nil {
-		return nil, err
-	}
-
 	return migrate.NewWithDatabaseInstance(
 		"file://"+config.MigrationsPath,
-		TestDbPass,
+		TestDbName,
 		driver,
 	)
 }
 
-func createMigrations(db *gorm.DB, config *migrations.Config) error {
+func createMigrations(db *mongo.Client, config *migrations.Config) error {
 	migration, err := setupMigrations(db, config)
 	if err != nil {
 		return fmt.Errorf("failed to setup migrations: %w", err)
@@ -114,17 +92,17 @@ func createMigrations(db *gorm.DB, config *migrations.Config) error {
 	return nil
 }
 
-func createDB(ctx context.Context, container testcontainers.Container, config *migrations.Config) (*gorm.DB, error) {
-	db, err := createGORM(ctx, container)
+func createDB(ctx context.Context, container testcontainers.Container, config *migrations.Config) (*mongo.Database, error) {
+	client, err := createMongo(ctx, container)
 	if err != nil {
 		return nil, err
 	}
 
-	if err = createMigrations(db, config); err != nil {
+	if err = createMigrations(client, config); err != nil {
 		return nil, err
 	}
 
-	return db, nil
+	return client.Database(TestDbName), nil
 }
 
 func NewTestDB(ctx context.Context, config *migrations.Config) (*TestDB, error) {
@@ -136,13 +114,15 @@ func NewTestDB(ctx context.Context, config *migrations.Config) (*TestDB, error) 
 	db, err := createDB(ctx, container, config)
 	if err != nil {
 		if err := container.Terminate(ctx); err != nil {
-			return nil, fmt.Errorf("failed to terminate database: %w", err)
+			return nil, fmt.Errorf("failed to terminate mongo: %w", err)
 		}
 		return nil, err
 	}
 
+	collection := db.Collection(TestCollectionName)
+
 	return &TestDB{
-		DB:        db,
-		container: container,
+		Collection: collection,
+		container:  container,
 	}, nil
 }
