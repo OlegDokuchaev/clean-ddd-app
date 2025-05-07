df7c6a8a0bd7a0e94680fa5a052270830c8aa3b9
dokuchaev <dokuchaev@akc.ru>
2025-05-07 15:05:05 +0300

    feat(order service infrastructure): update db functions

--- order/internal/infrastructure/db/db.go
+++ order/internal/infrastructure/db/db.go
@@ -1,23 +1,36 @@
 package db
 
 import (
+	"context"
 	"fmt"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func createDSN(c *Config) string {
-	return fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		c.Host,
-		c.Port,
-		c.Username,
-		c.Password,
-		c.Database,
-	)
+func NewConnection(cfg *Config) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI(cfg.URI)
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
+
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
+
+	return client, nil
 }
 
-func NewDB(config *Config) (*gorm.DB, error) {
-	dsn := createDSN(config)
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func NewDB(cfg *Config, client *mongo.Client) *mongo.Database {
+	return client.Database(cfg.Database)
+}
+
+func NewOrderCollection(db *mongo.Database, cfg *Config) *mongo.Collection {
+	return db.Collection(cfg.OrderCollection)
 }
