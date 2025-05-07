d09e413b463ac550bf40bee0f1cde8f9fc02a789
dokuchaev <dokuchaev@akc.ru>
2025-05-07 15:35:55 +0300

    build(courier service): update entrypoint.sh

--- courier/entrypoint.sh
+++ courier/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Running migrations..."
 DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable"
-migrate -path ${DB_MIGRATIONS_PATH} -database "$DATABASE_URL" up
+migrate -source "file://${DB_MIGRATIONS_PATH}" -database "$DATABASE_URL" up
 
 echo "Starting the app..."
 exec ./main
