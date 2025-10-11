2e81fc7d3660d9b92775a7741689d3aa3c584f82
dokuchaev <dokuchaev@akc.ru>
2025-10-11 12:59:54 +0300

    feat(warehouse domain): update product repository

--- warehouse/internal/domain/product/repository.go
+++ warehouse/internal/domain/product/repository.go
@@ -8,4 +8,5 @@ import (
 type Repository interface {
 	Create(ctx context.Context, product *Product) error
 	GetByID(ctx context.Context, productID uuid.UUID) (*Product, error)
+	GetAll(ctx context.Context, limit, offset int) ([]*Product, error)
 }
