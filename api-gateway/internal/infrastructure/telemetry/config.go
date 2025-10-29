eb90185c217b84f1af4658bda16d0116e5fcf677
dokuchaev <dokuchaev@akc.ru>
2025-10-29 16:05:52 +0300

    feat(api gateway infrastructure): implement telemetry di

---
+++ api-gateway/internal/infrastructure/telemetry/config.go
@@ -0,0 +1,25 @@
+package telemetry
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	ServiceName        string        `envconfig:"OTEL_SERVICE_NAME" required:"true"`
+	OtlpEndpoint       string        `envconfig:"OTEL_OTLP_ENDPOINT" required:"true"`
+	SamplerRatio       float64       `envconfig:"OTEL_SAMPLER_ARG" required:"true"`
+	BatchTimeout       time.Duration `envconfig:"OTEL_BATCH_TIMEOUT" required:"true"`
+	MaxQueueSize       int           `envconfig:"OTEL_MAX_QUEUE_SIZE" required:"true"`
+	MaxExportBatchSize int           `envconfig:"OTEL_MAX_EXPORT_BATCH_SIZE" required:"true"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, fmt.Errorf("failed to load telemetry config: %w", err)
+	}
+	return &cfg, nil
+}
---
+++ api-gateway/internal/infrastructure/telemetry/init.go
@@ -0,0 +1,50 @@
+package telemetry
+
+import (
+	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+)
+
+func Init(cfg *Config) (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	exp, err := otlptracegrpc.New(ctx,
+		otlptracegrpc.WithEndpoint(cfg.OtlpEndpoint),
+		otlptracegrpc.WithInsecure(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	res, _ := resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithTelemetrySDK(),
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(cfg.ServiceName),
+		),
+	)
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
+		sdktrace.WithSampler(sdktrace.ParentBased(
+			sdktrace.TraceIDRatioBased(cfg.SamplerRatio),
+		)),
+		sdktrace.WithBatcher(exp,
+			sdktrace.WithBatchTimeout(cfg.BatchTimeout),
+			sdktrace.WithMaxQueueSize(cfg.MaxQueueSize),
+			sdktrace.WithMaxExportBatchSize(cfg.MaxExportBatchSize),
+		),
+	)
+
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	))
+
+	return tp.Shutdown, nil
+}
