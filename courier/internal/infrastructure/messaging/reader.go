176c9fe967e6e7445c75a08404bf00544eeb6dd5
dokuchaev <dokuchaev@akc.ru>
2025-10-30 18:28:00 +0300

    feat(courier infrastructure): update reader messaging

--- courier/internal/infrastructure/messaging/reader.go
+++ courier/internal/infrastructure/messaging/reader.go
@@ -1,11 +1,28 @@
 package messaging
 
-import "github.com/segmentio/kafka-go"
+import (
+	otelkafkakonsumer "github.com/Trendyol/otel-kafka-konsumer"
+	"github.com/segmentio/kafka-go"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
 
-func NewCourierCmdReader(config *Config) *kafka.Reader {
-	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{config.Address},
-		GroupID: config.CourierCmdConsumerGroupID,
-		Topic:   config.CourierCmdTopic,
-	})
+func NewCourierCmdReader(config *Config, tp *sdktrace.TracerProvider) (*otelkafkakonsumer.Reader, error) {
+	return otelkafkakonsumer.NewReader(
+		kafka.NewReader(kafka.ReaderConfig{
+			Brokers: []string{config.Address},
+			GroupID: config.CourierCmdConsumerGroupID,
+			Topic:   config.CourierCmdTopic,
+		}),
+		otelkafkakonsumer.WithTracerProvider(tp),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String("CourierCmdTopic"),
+			},
+		),
+	)
 }
