4e5e3b7cd45f7a1de6c11f8aa9212038a0a2e67f
dokuchaev <dokuchaev@akc.ru>
2025-10-30 18:27:37 +0300

    feat(courier infrastructure): update writer messaging

--- courier/internal/infrastructure/messaging/writer.go
+++ courier/internal/infrastructure/messaging/writer.go
@@ -1,10 +1,27 @@
 package messaging
 
-import "github.com/segmentio/kafka-go"
+import (
+	otelkafkakonsumer "github.com/Trendyol/otel-kafka-konsumer"
+	"github.com/segmentio/kafka-go"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
 
-func NewCourierCmdResWriter(config *Config) *kafka.Writer {
-	return &kafka.Writer{
-		Addr:  kafka.TCP(config.Address),
-		Topic: config.CourierCmdResTopic,
-	}
+func NewCourierCmdResWriter(config *Config, tp *sdktrace.TracerProvider) (*otelkafkakonsumer.Writer, error) {
+	return otelkafkakonsumer.NewWriter(
+		&kafka.Writer{
+			Addr:  kafka.TCP(config.Address),
+			Topic: config.CourierCmdResTopic,
+		},
+		otelkafkakonsumer.WithTracerProvider(tp),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String("CourierCmdTopic"),
+			},
+		),
+	)
 }
