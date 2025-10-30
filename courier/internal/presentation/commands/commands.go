a0cfa100a8f9f45f9ea384ecd3d88a1086bf3800
dokuchaev <dokuchaev@akc.ru>
2025-10-30 18:40:40 +0300

    feat(courier presentation commands): implement telemetry

--- courier/internal/presentation/commands/commands.go
+++ courier/internal/presentation/commands/commands.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/google/uuid"
 )
@@ -16,6 +17,11 @@ type (
 		ID      uuid.UUID
 		Name    CmdMessageName
 		Payload json.RawMessage
+	}
+
+	CmdEnvelope struct {
+		Ctx context.Context
+		Cmd *CmdMessage
 	}
 )
 
