af091a6b98bba3502efa5b6e091a986058784bdf
dokuchaev <dokuchaev@akc.ru>
2025-10-31 12:02:31 +0300

    feat(warehouse infrastructure messaging): implement telemetry into readers

--- warehouse/internal/infrastructure/messaging/reader.go
+++ warehouse/internal/infrastructure/messaging/reader.go
@@ -1,19 +1,46 @@
 package messaging
 
-import "github.com/segmentio/kafka-go"
+import (
+	otelkafkakonsumer "github.com/Trendyol/otel-kafka-konsumer"
+	"github.com/segmentio/kafka-go"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+)
 
-func NewWarehouseCmdReader(config *Config) *kafka.Reader {
-	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{config.Address},
-		GroupID: config.WarehouseCmdConsumerGroupID,
-		Topic:   config.WarehouseCmdTopic,
-	})
+func NewWarehouseCmdReader(config *Config, tp *sdktrace.TracerProvider) (*otelkafkakonsumer.Reader, error) {
+	return otelkafkakonsumer.NewReader(
+		kafka.NewReader(kafka.ReaderConfig{
+			Brokers: []string{config.Address},
+			GroupID: config.WarehouseCmdConsumerGroupID,
+			Topic:   config.WarehouseCmdTopic,
+		}),
+		otelkafkakonsumer.WithTracerProvider(tp),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String(config.WarehouseCmdTopic),
+			},
+		),
+	)
 }
 
-func NewProductEventReader(config *Config) *kafka.Reader {
-	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{config.Address},
-		GroupID: config.ProductEventConsumerGroupID,
-		Topic:   config.ProductEventTopic,
-	})
+func NewProductEventReader(config *Config, tp *sdktrace.TracerProvider) (*otelkafkakonsumer.Reader, error) {
+	return otelkafkakonsumer.NewReader(
+		kafka.NewReader(kafka.ReaderConfig{
+			Brokers: []string{config.Address},
+			GroupID: config.ProductEventConsumerGroupID,
+			Topic:   config.ProductEventTopic,
+		}),
+		otelkafkakonsumer.WithTracerProvider(tp),
+		otelkafkakonsumer.WithPropagator(propagation.TraceContext{}),
+		otelkafkakonsumer.WithAttributes(
+			[]attribute.KeyValue{
+				semconv.MessagingDestinationKindTopic,
+				semconv.MessagingKafkaClientIDKey.String(config.ProductEventTopic),
+			},
+		),
+	)
 }
